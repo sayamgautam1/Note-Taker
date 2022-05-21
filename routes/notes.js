04ccf0b18e600befd614772a2e8c645ab35bbdea
Sayam Gautam <sayamgautam1@gmail.com>
2022-05-21 19:14:50 +0930

    add post method to save new note inside db.json

--- routes/notes.js
+++ routes/notes.js
@@ -16,6 +16,20 @@ notes.get("/", (req, res) => {
 
 notes.post("/", (req, res) => {
   console.log(req.body);
+  //{ title: 'asdas', text: 'adads' }
+  const { title, text } = req.body;
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      nodeID: uuidv4(),
+    };
+
+    readAndAppend(newNote, "./db/db.json");
+    res.json(`Tip added successfully 🚀`);
+  } else {
+    res.error("Error in adding tip");
+  }
 });
 
 module.exports = notes;
